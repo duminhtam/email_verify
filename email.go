a1465caf683fc2cf7d7ec32dab20d84fbeaf6100
duminhtam <tamdu@chotot.vn>
2014-07-09 16:01:26 +0700

    update

--- email.go
+++ email.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
+	_ "github.com/lib/pq"
+	"database/sql"
 )
 
 func main(){
-	fmt.Print(VerifyEmail("tamdu@chotot.vn"))
+	db, err := sql.Open("postgres", "postgres://pqgotest:password@localhost/pqgotest?sslmode=verify-full")
+
+	if err != nil {
+		fmt.Print(err)
+	}
+	rows, err := db.Query("SELECT * FROM users")
+
+	for rows.Next(){
+		rows.Scan("a")
+	}
+//	fmt.Print(VerifyEmail("tamdu@chotot.vn"))
 }
 
 
